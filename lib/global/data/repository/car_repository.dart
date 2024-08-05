c787fa579bd24f9237a3725adbd16fe7ada17601
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-08-05 17:23:18 -0600

    [ADD] MODEL

--- lib/global/data/repository/car_repository.dart
+++ lib/global/data/repository/car_repository.dart
@@ -10,7 +10,7 @@ class CarRepository {
 
   Future<void> createCars(CarModel car) async {
     final response = await http.post(
-      Uri.parse('$apiUrl/cars/create'),
+      Uri.parse('https://ikj1ppx0z9.execute-api.us-east-1.amazonaws.com/Prod/post/'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer $accessToken',
@@ -40,7 +40,7 @@ class CarRepository {
 
   Future<void> updateCar(String id, CarModel car) async {
     final response = await http.put(
-      Uri.parse('$apiUrl/cars/update/$id'),
+      Uri.parse(' https://ikj1ppx0z9.execute-api.us-east-1.amazonaws.com/Prod/put/$id'),
       headers: <String, String>{
         'Authorization': 'Bearer $accessToken',
       },
@@ -54,11 +54,13 @@ class CarRepository {
 
   Future<void> deleteCar(String id) async {
     final response = await http.delete(
-      Uri.parse('$apiUrl/cars/delete/$id'),
+      Uri.parse(' https://ikj1ppx0z9.execute-api.us-east-1.amazonaws.com/Prod/delete'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer $accessToken',
       },
+            body: jsonEncode(id),
+
     );
 
     if (response.statusCode != 200) {
@@ -68,7 +70,7 @@ class CarRepository {
 
   Future<List<CarModel>> getAllCars() async {
     final response = await http.get(
-      Uri.parse('$apiUrl/get_data'),
+      Uri.parse('https://ikj1ppx0z9.execute-api.us-east-1.amazonaws.com/Prod/get/'),
       headers: <String, String>{
         'Authorization': 'Bearer $accessToken',
       },
@@ -76,7 +78,18 @@ class CarRepository {
 
     if (response.statusCode == 200) {
       List<dynamic> carsJson = jsonDecode(response.body);
-      return carsJson.map((json) => CarModel.fromJson(json)).toList();
+      // Map the list of tuples to a list of CarModel instances
+      return carsJson.map((json) {
+        print(json.toString());
+        Map<String, dynamic> carMap = {
+          'idCar': json[0],
+          'brand': json[1],
+          'model': json[2],
+          'cilindrada': json[3],
+          'color': json[4],
+        };
+        return CarModel.fromJson(carMap);
+      }).toList();
     } else {
       throw Exception('Error al cargar los autos');
     }
